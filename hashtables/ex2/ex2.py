3fca6415df1bcc6df572ee468eb1a087d73c8aad
Chelsea Wetzel <chelswetz@gmail.com>
2020-07-10 12:20:36 -0400

    problem two solved

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,30 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    ticket_hash = {}
+    route = []
+
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            route.append(ticket.destination)
+        ticket_hash[ticket.source] = ticket.destination
+
+    current_ticket = ticket_hash[route[0]]
+
+    count = 1
+
+    while count < length:
+        route.append(current_ticket)
+        current_ticket = ticket_hash[current_ticket]
+        count += 1
 
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_4 = Ticket("DCA", "PAN")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("PAN", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4]
+
+print(reconstruct_trip(tickets, 4))
